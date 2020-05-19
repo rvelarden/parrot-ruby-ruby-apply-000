7d11377b4fd88baf446451855ba9d437bb354d36
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-19 20:43:09 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot; "Squawk!"
-  puts "#{"Squawk!"}."
+  puts "#{answer}."
 end
-expect(parrot); ("Squawk!")
+
+puts "Squawk!"
+parrot("Squawk!")
