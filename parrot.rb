d9e8f6b894b89c8adf8897d8eeb1256a852afd65
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-19 21:12:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name = "Ruby programmer")
+def parrot(phrase = "Ruby programmer")
   puts "#{phrase}."
 end
